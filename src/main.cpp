be835e735c126a6a2f3e25e4085c381e4ae45384
programmer3481 <gwch3481@gmail.com>
2022-10-09 17:54:31 +0900

    command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,11 @@ int main(int argc, char *argv[]) {
     int width = (consoleWidth-3)/2;
     int height = consoleHeight-2;
     if (argc >= 2) {
-        width = std::stoi(argv[0]);
-        height = std::stoi(argv[1]);
+        width = std::stoi(argv[1]);
+        height = std::stoi(argv[2]);
+    }
+    if (width <= 3 || height <= 3) {
+        return 0;
     }
     int mines = (width * height)/8;
     cmdSweeper::CmdSweeper sweeper(width, height, mines, getch);
